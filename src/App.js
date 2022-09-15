d6ab1c0aa220c8782ed4cf0ffd878593eeaf4291
sandeep <sandeep@GMAIL.COM>
2022-09-15 22:44:16 +0530

    'remove'

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ import { Routes,Route } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
-      <div></div>
       <Navbar />
       <Routes>
         <Route path='/Try' element={<Try />} />
